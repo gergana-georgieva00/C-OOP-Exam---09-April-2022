594b50f1b3a07e39c54b6f859d9f0288f9d1238e
Gergana Georgieva <gerganagergana1@gmail.com>
2022-12-01 16:01:31 +0200

    C# OOP Exam - 09 April 2022

---
+++ Formula1/Repositories/RaceRepository.cs
@@ -0,0 +1,31 @@
+﻿using Formula1.Models;
+using Formula1.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Formula1.Repositories
+{
+    public class RaceRepository : IRepository<Race>
+    {
+        private List<Race> models;
+
+        public RaceRepository()
+        {
+            this.models = new List<Race>();
+        }
+
+        public IReadOnlyCollection<Race> Models => this.models.AsReadOnly();
+
+        public void Add(Race model)
+        {
+            this.models.Add(model);
+        }
+
+        public Race FindByName(string name)
+            => this.models.Find(r => r.RaceName == name);
+
+        public bool Remove(Race model)
+            => this.models.Remove(model);
+    }
+}
