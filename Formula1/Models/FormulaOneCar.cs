8c226bdef9c155d307888fe6e18393265447b5ff
Gergana Georgieva <gerganagergana1@gmail.com>
2022-12-01 13:47:34 +0200

    C# OOP Exam - 09 April 2022

---
+++ Formula1/Models/FormulaOneCar.cs
@@ -0,0 +1,54 @@
+﻿using Formula1.Models.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Formula1.Models
+{
+    public abstract class FormulaOneCar : IFormulaOneCar
+    {
+        string model;
+        int horsePower;
+        double engineDisplacement;
+
+        public FormulaOneCar(string model, int horsePower, double engineDisplacement)
+        {
+            this.Model = model;
+            this.Horsepower = horsePower;
+            this.EngineDisplacement = engineDisplacement;
+        }
+
+        public string Model 
+        {
+            get => this.model;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value) || value.Length < 3)
+                    throw new ArgumentException($"Invalid car model: {value}.");
+                this.model = value;
+            } 
+        }
+
+        public int Horsepower
+        {
+            get => this.horsePower;
+            private set
+            {
+                if (value < 900 || value > 1050)
+                    throw new ArgumentException($"Invalid car horsepower: {value}.");
+                this.horsePower = value;
+            }
+        }
+
+        public double EngineDisplacement
+        {
+            get;
+            private set;
+        }
+
+        public double RaceScoreCalculator(int laps)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
