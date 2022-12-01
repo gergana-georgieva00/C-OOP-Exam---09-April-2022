d2c7ccfbfce2180bb32002b26b29691efc1e8c55
Gergana Georgieva <gerganagergana1@gmail.com>
2022-12-01 13:50:27 +0200

    C# OOP Exam - 09 April 2022

---
+++ Formula1/Models/Contracts/Williams.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Formula1.Models.Contracts
+{
+    public class Williams : FormulaOneCar
+    {
+        public Williams(string model, int horsePower, double engineDisplacement) : base(model, horsePower, engineDisplacement)
+        {
+        }
+    }
+}
