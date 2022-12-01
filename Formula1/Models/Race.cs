331aade28367311e751d58a1a4272854c0876aac
Gergana Georgieva <gerganagergana1@gmail.com>
2022-12-01 14:05:09 +0200

    C# OOP Exam - 09 April 2022

---
+++ Formula1/Models/Race.cs
@@ -0,0 +1,55 @@
+﻿using Formula1.Models.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Formula1.Models
+{
+    public class Race : IRace
+    {
+        private string raceName;
+        private int numberOfLaps;
+
+        public Race(string raceName, int numberOfLaps)
+        {
+            this.RaceName = raceName;
+            this.NumberOfLaps = numberOfLaps;
+        }
+
+        public string RaceName 
+        {
+            get => this.raceName;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value) || value.Length < 5)
+                    throw new ArgumentException($"Invalid race name: {value}.");
+                this.raceName = value;
+            }
+        }
+
+        public int NumberOfLaps
+        {
+            get => this.numberOfLaps;
+            private set
+            {
+                if (value < 1)
+                    throw new ArgumentException($"Invalid lap numbers: {value}.");
+                this.numberOfLaps = value;
+            }
+        }
+
+        public bool TookPlace  { get; set; }
+
+        public ICollection<IPilot> Pilots { get; private set; }
+
+        public void AddPilot(IPilot pilot)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string RaceInfo()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
